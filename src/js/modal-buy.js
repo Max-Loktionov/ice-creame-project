3e9b482cc698fdb1ce3c4325a22c5c0b06590d50
Max-Loktionov <studreno@gmail.com>
2022-05-22 14:54:43 +0300

    group-project
    -html
    -scss
    -js
    -parcel,node
    -base markup, modal window, backdrop, transition. adaptive image, BEM

---
+++ src/js/modal-buy.js
@@ -0,0 +1,41 @@
+(() => {
+  const refs = {
+   
+    openModalBuy: document.querySelector('.modal-buy--open'),
+    closeModalBuy: document.querySelector('.modal-buy--close'),
+    modalBuy: document.querySelector('.modal-buy__offer'), 
+
+    openModalBuyMenu: document.querySelector('.modal-buy-menu--open'),
+    closeModalBuyMenu: document.querySelector('.modal-buy-menu--close'),
+    modalBuyMenu: document.querySelector('.modal-buy-menu__offer'),
+
+   
+    };
+
+
+
+  refs.openModalBuy.addEventListener('click', toggleModal);
+  refs.closeModalBuy.addEventListener('click', toggleModal); 
+
+  refs.openModalBuyMenu.addEventListener('click', toggleModal);
+  refs.closeModalBuyMenu.addEventListener('click', toggleModal); 
+
+ 
+  window.addEventListener('click', e => { 
+     
+    const target = e.target 
+     if (!target.closest('.modal-buy') && !target.closest('.button__buy')) {
+       refs.modalBuyMenu.classList.add('visually-hidden')
+       document.body.classList.remove('modal-buy-menu--visible');
+     }
+     });
+
+  function toggleModal() {
+
+    document.body.classList.toggle('modal-buy-menu--visible');
+    refs.modalBuyMenu.classList.toggle('visually-hidden');  
+
+  }
+  
+
+})();
---
+++ src/js/modal.js
@@ -0,0 +1,49 @@
+
+
+(() => {
+  const mobileMenu = document.querySelector('.mobile-menu--js');
+  const openMenuBtn = document.querySelector('.mobile-menu__open--js');
+  const closeMenuBtn = document.querySelector('.mobile-menu__close--js');
+  const headerBackdrop = document.querySelector('.header__backdrop');
+
+  const openMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.add('mobile-menu--is-open');
+    openMenuBtn.classList.add('--is-open')
+    document.body.classList.add('menu--is-open');
+    headerBackdrop.classList.remove('visually-hidden')
+  };
+
+  openMenuBtn.addEventListener('click', openMenu);
+  closeMenuBtn.addEventListener('click', openMenu);
+
+  function closeMenu() {
+      mobileMenu.classList.remove('mobile-menu--is-open');
+      document.body.classList.remove('menu--is-open');
+      headerBackdrop.classList.add('visually-hidden')
+  }
+
+  window.addEventListener('keydown', e => {
+    if (e.key === 'Escape' || ' ') {
+      openMenuBtn.setAttribute('aria-expanded', false);
+      closeMenu();
+         }
+  });
+  
+  window.addEventListener('click', e => { 
+     
+    const target = e.target 
+     if (!target.closest('.mobile-menu') && !target.closest('.button__buy')&& !target.closest('.mobile-menu__switch')) {
+       closeMenu()
+     }
+     });
+
+     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+       if (!e.matches) return;
+       openMenuBtn.setAttribute('aria-expanded', false);
+
+      closeMenu()
+      
+    });
+  })();
