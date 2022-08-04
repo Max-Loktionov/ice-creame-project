323298380a4a9b8c98c82b5ebc18bcae08d5f9b9
Max-Loktionov <studreno@gmail.com>
2022-08-04 20:17:58 +0300

    fix modal for iphone

--- src/js/contact-modal.js
+++ src/js/contact-modal.js
@@ -1,41 +1,64 @@
+'use strict';
+// Это комментарий. Дальше идет весь код JS-файла
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-contact-modal-open]'),
-    openModaBtn: document.querySelector('[data-contact-franchise-modal-open]'),
+    openLocationModalBtn: document.querySelector('[data-contact-locations-modal-open]'),
+    openFranchiseModalBtn: document.querySelector('[data-contact-franchise-modal-open]'),
+
     closeModalBtn: document.querySelector('[data-contact-modal-close]'),
     contactModal: document.querySelector('[data-contact-modal]'),
-    // openModalBuy: document.querySelector('.modal-buy--open'),
-    // closeModalBuy: document.querySelector('.modal-buy--close'),
-    // modalBuy: document.querySelector('.modal-buy__offer'),
-    
-   
 
+    ModalFranchiseBtn: document.querySelector('.contact-franchise-modal'),
   };
+  console.log('contactModal', refs.contactModal);
+  console.log('openLocationModalBtn', refs.openLocationModalBtn);
+  console.log('openFranchiseModalBtn', refs.openFranchiseModalBtn);
+  console.log('closeModalBtn', refs.closeModalBtn);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openLocationModalBtn.addEventListener('click', toggleModal);
+  refs.openFranchiseModalBtn.addEventListener('click', toggleModaFr);
+  refs.ModalFranchiseBtn.addEventListener('click', toggleTitle);
 
-
-
-  refs.closeModalBtn.addEventListener('click', toggleModaFr);
-  refs.openModaBtn.addEventListener('click', toggleModal);
-
-
-// document.addEventListener('touchstart', onTouchStart, {passive: true});
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle('contact-modal-open');
-    
-    refs.contactModal.classList.toggle('visually-hidden');
 
-    // refs.modalBuy.classList.toggle('visually-hidden');
-    
+    refs.contactModal.classList.toggle('contact_modal__backdrop--hidden');
+    let paragraph = document.querySelector('.js-contact-modal__title');
+    paragraph.textContent = 'Our Location';
   }
 
   function toggleModaFr() {
+    refs.contactModal.classList.toggle('contact_modal__backdrop--hidden');
+
     document.body.classList.toggle('contact-modal-open');
-    refs.contactModal.classList.toggle('visually-hidden');
+
     let paragraph = document.querySelector('.js-contact-modal__title');
-    paragraph.textContent = 'Franchise!';    
+    paragraph.textContent = 'Franchise!';
   }
-  
 })();
+
+function toggleTitle() {
+  let paragraph = document.querySelector('.js-contact-modal__title');
+  paragraph.textContent = 'Franchise!';
+}
+
+// По примеру Макса
+// (() => {
+//     const refs = {
+
+//       openModalContact: document.querySelector('.data-contact-modal-open'),
+//       closeModalContact: document.querySelector('.data-contact-modal-close'),
+//       modalContact: document.querySelector('.data-contact-modal'),
+
+//       };
+
+//     refs.openModalContact.addEventListener('click', toggleModal);
+//     refs.closeModalContact.addEventListener('click', toggleModal);
+
+//     function toggleModal() {
+
+//       refs.modalContact.classList.toggle('visually-hidden');
+//       document.body.classList.toggle('modal-about-menu--visible');
+//     }
--- src/js/modal-about.js
+++ src/js/modal-about.js
@@ -1,31 +1,25 @@
+'use strict';
+// Это комментарий. Дальше идет весь код JS-файла
 (() => {
-    const refs = {
-     
-        
-      openModalAboutMenu: document.querySelector('.modal-about-menu--open'),
-      closeModalAboutMenu: document.querySelector('.modal-about-menu--close'),
-      modalAboutMenu: document.querySelector('.modal-about__offer'),
-    
-      };
-  
-  
-    
-    refs.openModalAboutMenu.addEventListener('click', toggleModal);
-    refs.closeModalAboutMenu.addEventListener('click', toggleModal);
-  
-   window.addEventListener('click', e => { 
-     
-    const target = e.target 
-     if (!target.closest('.modal-about') && !target.closest('.modal-about-menu--open')) {
-       refs.modalAboutMenu.classList.add('visually-hidden')
-       document.body.classList.remove('modal-about-menu--visible');
-     }
-     });
-  
-    function toggleModal() {
-     
-      refs.modalAboutMenu.classList.toggle('visually-hidden');
-      document.body.classList.toggle('modal-about-menu--visible');
+  const refs = {
+    openModalAboutMenu: document.querySelector('.modal-about-menu--open'),
+    closeModalAboutMenu: document.querySelector('.modal-about-menu--close'),
+    modalAboutMenu: document.querySelector('.modal-about__offer'),
+  };
+
+  refs.openModalAboutMenu.addEventListener('click', toggleModal);
+  refs.closeModalAboutMenu.addEventListener('click', toggleModal);
+
+  window.addEventListener('click', e => {
+    const target = e.target;
+    if (!target.closest('.modal-about') && !target.closest('.modal-about-menu--open')) {
+      refs.modalAboutMenu.classList.add('visually-hidden');
+      document.body.classList.remove('modal-about-menu--visible');
     }
-  
-  })();
+  });
+
+  function toggleModal() {
+    refs.modalAboutMenu.classList.toggle('visually-hidden');
+    document.body.classList.toggle('modal-about-menu--visible');
+  }
+})();
